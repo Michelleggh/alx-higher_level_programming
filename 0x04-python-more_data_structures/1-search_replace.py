a19c02cd94b99bcd8c63d8192c68b53ba4dc675f
Michelle Muhoro <muhoromichelle@gmail.com>
2022-07-08 06:34:13 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-<<<<<<< HEAD
     copy = my_list.copy()
     for i in range(len(my_list)):
         if my_list[i] == search:
             copy[i] = replace
     return copy
-=======
-copy = my_list.copy()
-for i in range(len(my_list)):
-if my_list[i] == search:
-copy[i] = replace
-return copy
->>>>>>> 9fe8971604197c0f07d9f1a59cb46be84a35ad02
